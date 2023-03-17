f265d43efe6d169dff54526119f0e4ce639d823d
ou guangye <3250679635@qq.com>
2023-03-17 19:50:48 +0800

    完成教师dashboard页面

--- score_web/src/routes/index.ts
+++ score_web/src/routes/index.ts
@@ -1,11 +1,16 @@
 import {RouteConfig} from  'react-router-config'
 import Login from '@/pages/Login/Login'
-
+import TeacherDashBoard from '@/pages/Teacher/DashBoard/DashBoard'
 const routes:RouteConfig = [
     {
         path: '/login',
         exact: true,
         component: Login,
+    },
+    {
+        path: '/teacher',
+        exact: true,
+        component:TeacherDashBoard 
     }
 ]
 
