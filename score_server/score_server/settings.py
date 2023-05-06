f674a67d96b7604a395982cc4874a4601d2dc099
ou guangye <3250679635@qq.com>
2023-05-06 17:44:38 +0800

    打包前端文件，并且修改了django相关设置

--- score_server/score_server/settings.py
+++ score_server/score_server/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'score_server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["build"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,8 +119,12 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
 # Media 路径
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# 前端路径
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static')
+]
