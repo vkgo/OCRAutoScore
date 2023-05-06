90af2ca77a8111d795e4d0ee70c76c2fd12cd7c7
vkgo <hwjho@qq.com>
2023-05-06 18:07:26 +0800

    解决冲突

--- score_server/score_server/urls.py
+++ score_server/score_server/urls.py
@@ -15,14 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include,re_path
+from django.urls import path, include
 from django.views.static import serve
-from django.views.generic import TemplateView
+
 from score_server import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include('index.urls')),
     url(r'media/(?P<path>.*)', serve, {"document_root": settings.MEDIA_ROOT}),
-    re_path("^$", TemplateView.as_view(template_name="index.html"))
 ]
